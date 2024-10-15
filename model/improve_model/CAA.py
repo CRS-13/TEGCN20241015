41ba2ff9eb6e6dbbe3992cf4ca33b2cdc59d36fb
CRS-13 <236717891@qq.com>
2024-10-15 21:39:34 +0800

    Update CAA.py

--- model/improve_model/CAA.py
+++ model/improve_model/CAA.py
@@ -1,11 +1,7 @@
 from typing import Optional
 import torch.nn as nn
 import torch
-# 论文地址：https://arxiv.org/pdf/2403.06258
-# 论文：Poly Kernel Inception Network for Remote Sensing Detection(CVPR 2024)
-# Github地址：https://github.com/NUST-Machine-Intelligence-Laboratory/PKINet
-# 全网最全100➕即插即用模块GitHub地址：https://github.com/ai-dawang/PlugNPlay-Modules
-# Context Anchor Attention (CAA) module
+
 class ConvModule(nn.Module):
     def __init__(
             self,
@@ -19,17 +15,17 @@ class ConvModule(nn.Module):
             act_cfg: Optional[dict] = None):
         super().__init__()
         layers = []
-        # Convolution Layer
+       
         layers.append(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=(norm_cfg is None)))
-        # Normalization Layer
+        
         if norm_cfg:
             norm_layer = self._get_norm_layer(out_channels, norm_cfg)
             layers.append(norm_layer)
-        # Activation Layer
+        
         if act_cfg:
             act_layer = self._get_act_layer(act_cfg)
             layers.append(act_layer)
-        # Combine all layers
+        
         self.block = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -38,7 +34,7 @@ class ConvModule(nn.Module):
     def _get_norm_layer(self, num_features, norm_cfg):
         if norm_cfg['type'] == 'BN':
             return nn.BatchNorm2d(num_features, momentum=norm_cfg.get('momentum', 0.1), eps=norm_cfg.get('eps', 1e-5))
-        # Add more normalization types if needed
+        
         raise NotImplementedError(f"Normalization layer '{norm_cfg['type']}' is not implemented.")
 
     def _get_act_layer(self, act_cfg):
@@ -46,7 +42,7 @@ class ConvModule(nn.Module):
             return nn.ReLU(inplace=True)
         if act_cfg['type'] == 'SiLU':
             return nn.SiLU(inplace=True)
-        # Add more activation types if needed
+       
         raise NotImplementedError(f"Activation layer '{act_cfg['type']}' is not implemented.")
 
 class CAA(nn.Module):
@@ -76,9 +72,9 @@ class CAA(nn.Module):
         attn_factor = self.act(self.conv2(self.v_conv(self.h_conv(self.conv1(self.avg_pool(x))))))
         return attn_factor
 
-# Example usage to print input and output shapes
+
 if __name__ == "__main__":
-    input = torch.randn(1, 64, 128, 128) #输入 B C H W
+    input = torch.randn(1, 64, 128, 128)
     block = CAA(64)
     output = block(input)
     print(input.size())
